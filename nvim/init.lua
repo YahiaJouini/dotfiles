00d7057bfcb1bc534ec3325a25931be99fbf7ffa
YahiaJouini <jouiniyahya117@gmail.com>
2026-05-03 19:56:46 +0100

    kitty & nvim: control kitty padding from nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,28 @@
 require("vim._core.ui2").enable({
-	enable = true,
-	msg = {
-		target = "cmd", -- Routes all messages to the cmdline area
-		pager = { height = 0.4 },
-		msg = { height = 0.5, timeout = 3000 },
-		dialog = { height = 0.5 },
-		cmd = { height = 0.5 },
-	},
+  enable = true,
+  msg = {
+    target = "cmd", -- Routes all messages to the cmdline area
+    pager = { height = 0.4 },
+    msg = { height = 0.5, timeout = 3000 },
+    dialog = { height = 0.5 },
+    cmd = { height = 0.5 },
+  },
+})
+
+local kitty_padding_group = vim.api.nvim_create_augroup("KittyPadding", { clear = true })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = kitty_padding_group,
+  callback = function()
+    os.execute("kitty @ set-spacing padding=0")
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  group = kitty_padding_group,
+  callback = function()
+    os.execute("kitty @ set-spacing padding=10")
+  end,
 })
 
 require("config.options")
